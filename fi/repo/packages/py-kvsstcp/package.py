b9d7aaa3cab13fb01045b63dc1618b2e3ea3049c
Dylan Simon <dylan@dylex.net>
2021-09-15 23:00:32 -0400

    fi: add kvsstcp 1.2 release

--- fi/repo/packages/py-kvsstcp/package.py
+++ fi/repo/packages/py-kvsstcp/package.py
@@ -29,4 +29,5 @@ class PyKvsstcp(PythonPackage):
     homepage = "https://github.com/flatironinstitute/kvsstcp"
     url      = "https://github.com/flatironinstitute/kvsstcp/archive/refs/tags/1.1.tar.gz"
 
+    version('1.2', sha256='022ac2c03234dc9e3a921edf6015caa246fa7faf3ec0bf70511fc1bc94036cf5')
     version('1.1', sha256='c2ffc1077055626610995d71bad9028da03181a3e4c89a3c0eda0c9db8d06fe5')
