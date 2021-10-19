431b7aac5d886e75832a32d0d9acfa653db31845
Dylan Simon <dylan@dylex.net>
2021-10-19 16:49:23 -0400

    fi: lmod: put all module init in setup

--- fi/lmod/setup.csh
+++ fi/lmod/setup.csh
@@ -1,9 +1,12 @@
-set MODULEPATH_DEFAULT=@MODS@/Core
-setenv LMOD_PACKAGE_PATH @SITE@/site
-setenv LMOD_RC @SITE@/lmodrc.lua
-setenv LMOD_SITE_MSG_FILE @SITE@/site_msg.lua
-setenv LMOD_AVAIL_STYLE 'system:<trim>:group'
-setenv LMOD_AVAIL_EXTENSIONS no
+if (! ${?ENABLE_LMOD} ) then
+  setenv ENABLE_LMOD 0
+endif
+
+if ( $ENABLE_LMOD == 1 ) then
+  set MODULEPATH_DEFAULT=@MODS@/Core
+else
+  set MODULEPATH_DEFAULT=/cm/shared/sw/modules
+endif
 
 if ( ! $?MODULEPATH || $MODULEPATH == /cm/shared/sw/modules ) then
   setenv MODULEPATH $MODULEPATH_DEFAULT
@@ -12,11 +15,26 @@ else if ( ":${MODULEPATH}:" !~ *":${MODULEPATH_DEFAULT}:"* ) then
 endif
 unset MODULEPATH_DEFAULT
 
-source @LMOD@/lmod/lmod/init/cshrc
-if ( ! $?__Init_Default_Modules ) then
-  setenv __Init_Default_Modules 1
-  setenv LMOD_SYSTEM_DEFAULT_MODULES slurm:openblas
-  module --initial_load restore
+if ( $ENABLE_LMOD == 1 ) then
+  setenv LMOD_PACKAGE_PATH @SITE@/site
+  setenv LMOD_RC @SITE@/lmodrc.lua
+  setenv LMOD_SITE_MSG_FILE @SITE@/site_msg.lua
+  setenv LMOD_AVAIL_STYLE 'system:<trim>:group'
+  setenv LMOD_AVAIL_EXTENSIONS no
+
+  source @LMOD@/lmod/lmod/init/cshrc
+
+  if ( ! $?__Init_Default_Modules ) then
+    setenv __Init_Default_Modules 1
+    setenv LMOD_SYSTEM_DEFAULT_MODULES slurm:openblas
+    module --initial_load restore
+  else
+    module refresh
+  endif
 else
-  module refresh
+  if ($?tcsh) then
+    source /cm/local/apps/environment-modules/current/Modules/default/init/tcsh
+  else
+    source /cm/local/apps/environment-modules/current/Modules/default/init/csh
+  endif
 endif
--- fi/lmod/setup.sh
+++ fi/lmod/setup.sh
@@ -1,12 +1,14 @@
-MODULEPATH_DEFAULT=@MODS@/Core
-export LMOD_PACKAGE_PATH=@SITE@/site
-export LMOD_RC=@SITE@/lmodrc.lua
-export LMOD_SITE_MSG_FILE=@SITE@/site_msg.lua
-export LMOD_AVAIL_STYLE='system:<trim>:group'
-# TODO: working around configuration bug (remove when fixed):
-export LMOD_AVAIL_EXTENSIONS=no
+if [[ -z $ENABLE_LMOD ]] ; then
+  export ENABLE_LMOD=0
+fi
 
-if [[ -z $MODULEPATH || $MODULEPATH = /cm/shared/sw/modules ]] ; then
+if [[ $ENABLE_LMOD = 1 ]] ; then
+  MODULEPATH_DEFAULT=@MODS@/Core
+else
+  MODULEPATH_DEFAULT=/cm/shared/sw/modules
+fi
+
+if [[ -z $MODULEPATH || $MODULEPATH == /cm/shared/sw/modules ]] ; then
   MODULEPATH=$MODULEPATH_DEFAULT
 elif [[ :$MODULEPATH: != *:$MODULEPATH_DEFAULT:* ]] ; then
   MODULEPATH=$MODULEPATH:$MODULEPATH_DEFAULT
@@ -14,12 +16,28 @@ fi
 unset MODULEPATH_DEFAULT
 export MODULEPATH
 
-. @LMOD@/lmod/lmod/init/profile
-if [[ -z $__Init_Default_Modules ]]; then
-  export __Init_Default_Modules=1
-  # TODO: do we want to set defaults in another way?
-  export LMOD_SYSTEM_DEFAULT_MODULES=${LMOD_SYSTEM_DEFAULT_MODULES:-slurm:openblas}
-  module --initial_load restore
+if [[ $ENABLE_LMOD = 1 ]] ; then
+  export LMOD_PACKAGE_PATH=@SITE@/site
+  export LMOD_RC=@SITE@/lmodrc.lua
+  export LMOD_SITE_MSG_FILE=@SITE@/site_msg.lua
+  export LMOD_AVAIL_STYLE='system:<trim>:group'
+  # TODO: working around configuration bug (remove when fixed):
+  export LMOD_AVAIL_EXTENSIONS=no
+
+  . @LMOD@/lmod/lmod/init/profile
+  if [[ -z $__Init_Default_Modules ]]; then
+    export __Init_Default_Modules=1
+    # TODO: do we want to set defaults in another way?
+    export LMOD_SYSTEM_DEFAULT_MODULES=${LMOD_SYSTEM_DEFAULT_MODULES:-slurm:openblas}
+    module --initial_load restore
+  else
+    module refresh
+  fi
 else
-  module refresh
+  case "$0" in
+      -bash|bash|*/bash) . /cm/local/apps/environment-modules/current/Modules/default/init/bash ;;
+	 -ksh|ksh|*/ksh) . /cm/local/apps/environment-modules/current/Modules/default/init/ksh ;;
+	 -zsh|zsh|*/zsh) . /cm/local/apps/environment-modules/current/Modules/default/init/zsh ;;
+		      *) . /cm/local/apps/environment-modules/current/Modules/default/init/sh ;; # sh and default for scripts
+  esac
 fi
