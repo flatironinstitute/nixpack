fadbb8c4e02d9a664acffb6a45b69f1cfc1dfd25
Dylan Simon <dylan@dylex.net>
2021-11-04 20:00:14 -0400

    workaround paraview build bug

--- fi/repo/packages/paraview/package.py
+++ fi/repo/packages/paraview/package.py
@@ -3,6 +3,10 @@ import spack.pkg.builtin.paraview
 import os
 
 class Paraview(spack.pkg.builtin.paraview.Paraview):
+    def cmake_args(self):
+        return [x.upper() if x.startswith('-DPARAVIEW_BUILD_EDITION') else x
+                for x in super().cmake_args()]
+
     @run_after('install')
     def install_wrapper(self):
         paraview_src = os.path.join(self.prefix.bin, 'paraview')
