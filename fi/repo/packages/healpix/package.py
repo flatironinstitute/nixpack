b4fe3945ac119d827c268121540805aa575f968d
Lehman Garrison <lgarrison@flatironinstitute.org>
2023-02-03 12:10:20 -0500

    fi: healpix cxx and sharp

--- fi/repo/packages/healpix/package.py
+++ fi/repo/packages/healpix/package.py
@@ -11,49 +11,33 @@ class Healpix(MakefilePackage):
     version("3.82", sha256="47629f057a2daf06fca3305db1c6950edb9e61bbe2d7ed4d98ff05809da2a127")
 
     depends_on("cfitsio")
-    # depends_on("libsharp", type="build")
 
     def edit(self, spec, prefix):
-        pass
-
-    def install(self, spec, prefix):
-        pass
-
+        config = FileFilter('hpxconfig_functions.sh')
+        config.filter(r'^\s*SHARPPREFIX=.*', f'SHARPPREFIX={prefix}')
+        config.filter(r'^\s*CXXPREFIX=.*', f'CXXPREFIX={prefix}')
+        config.filter(r'SHARP_LIBS="[^"]*"', f'SHARP_LIBS="-L{prefix.lib} -lsharp"')
+        config.filter(r'SHARP_CFLAGS="[^"]*"', f'SHARP_CFLAGS="-I{prefix.include}"')
 
     @run_before('build')
     def configure(self):
         spec = self.spec
-        prefix = self.prefix
-        
-        # configure_fix = FileFilter("hpxconfig_functions.sh")
-        # configure_fix.filter(
-        #     r"makeTopConf\(\){",
-        #     'makeTopConf(){ set',
-        # )
 
-        env = {}
         configure = Executable('./configure')
         configure(*self.configure_args(),
             extra_env=self.configure_env(spec),
-            _dump_env=env,
         )
-        # print(env)
-
-        makedirs(prefix.lib)
-        makedirs(prefix.include)
 
     def configure_args(self):
-        return ['-L', '--auto=c']  #  ,cxx,f90,
+        return ['-L', '--auto=c,cxx']
 
     def configure_env(self, spec):
         return dict(
             SHELL='sh',
             C_SHARED='1',
-            #FC
-            #F_SHARED
             FITSDIR=spec['cfitsio'].prefix.lib,
             FITSINC=spec['cfitsio'].prefix.include,
-            #PYTHON
+            SHARP_COPT='-O3 -ffast-math',
         )
         
     @property
@@ -65,13 +49,5 @@ class Healpix(MakefilePackage):
             f'C_INCDIR={prefix.include}'
         ]
 
-    # def patch(self):
-    #     spec = self.spec
-    #     configure_fix = FileFilter("configure")
-    #     # Link libsharp static libs
-    #     configure_fix.filter(
-    #         r"^SHARP_LIBS=.*$",
-    #         'SHARP_LIBS="-L{0} -lsharp -lc_utils -lfftpack -lm"'.format(
-    #             spec["libsharp"].prefix.lib
-    #         ),
-    #     )
+    def install(self, spec, prefix):
+        pass
