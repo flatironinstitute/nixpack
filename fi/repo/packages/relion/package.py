6260e87d0d7d40ba9ce0c0686c12c68d9d1e2bf0
Lehman Garrison <lgarrison@flatironinstitute.org>
2023-06-28 13:08:24 -0400

    fi: relion: fix hash

--- fi/repo/packages/relion/package.py
+++ fi/repo/packages/relion/package.py
@@ -5,5 +5,5 @@ import spack.pkg.builtin.relion as builtin
 class Relion(builtin.Relion):
 
     version("4.0.1",
-            sha256="0987e684e9d2dfd630f1ad26a6847493fe9fcd829ec251d8bc471d11701d51dd",
+            sha256="7e0d56fd4068c99f943dc309ae533131d33870392b53a7c7aae7f65774f667be",
     )
