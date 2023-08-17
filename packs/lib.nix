afd3a412e48dec28a758c77f6aa3a94c3a60c89b
Dylan Simon <dylan@dylex.net>
2023-08-16 21:20:25 -0400

    maybe slightly better/simpler fix for previous

--- packs/lib.nix
+++ packs/lib.nix
@@ -228,10 +228,7 @@ rec {
       intersectors = {
         version = versionsIntersect;
         variants = mergeWith (a: b: if a == b then a else
-          if isList a then if isList b then union a b
-                                       else union a [b]
-                      else if isList b then union [a] b
-                                       else [a b]);
+          union (toList a) (toList b));
         patches = a: b: a ++ b;
         depends = mergeWith prefsIntersect;
         extern = scalar;
