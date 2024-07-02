79ca987f68ec68958635c6bfad50c13aaa67e408
Dylan Simon <dylan@dylex.net>
2024-07-02 05:10:26 -0400

    fi: add lmod 8.7.43 to fix #701 for intel-oneapi-mkl
    which is broken in the first place

--- fi/repo/packages/lmod/package.py
+++ fi/repo/packages/lmod/package.py
@@ -2,6 +2,7 @@ import spack.pkg.builtin.lmod as builtin
 
 class Lmod(builtin.Lmod):
     patch("no-sys-tcl.patch")
+    version("8.7.43", sha256="d3fc792d9ca4243ef5a3128894b8da40414f06f9b9ea68bc4b37c4ebb90bd41f")
 
     def configure_args(self):
         args = super().configure_args()
