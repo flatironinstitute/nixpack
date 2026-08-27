36fc7b908b3c6ccb3c7c305ce140e532a0eaaca1
Dylan Simon <dylan@dylex.net>
2026-08-27 12:47:22 -0400

    fi: add pyslurm 26.05.x

--- fi/repo/packages/py_pyslurm/package.py
+++ fi/repo/packages/py_pyslurm/package.py
@@ -9,6 +9,7 @@ class PyPyslurm(PythonPackage):
     url = "https://github.com/PySlurm/pyslurm/archive/refs/tags/v24.11.0.tar.gz"
     git = "https://github.com/PySlurm/pyslurm"
 
+    version("26.05.x", branch="26.05.x", commit="6242fadde394ee3f00f5f96ac4fa4a05881db8b6")
     version("24.11.x", branch="24.11.x", commit="2adb2da8b5a1ca5d1b94d94858dd4c406f9f2f07")
     version("24.11.0", sha256="77d97c42bf3639f4babdfcbaa7e674351d974e10d85a7d0015cafd342b15f769")
 
